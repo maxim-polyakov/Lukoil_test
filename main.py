d2f59401b76aef4d1f38d2ecd0647b5ae4aed0dd
maxim-polyakov <maxim7012@mail.ru>
2024-11-30 17:38:56 +0300

    logging has been added

--- main.py
+++ main.py
@@ -2,131 +2,126 @@ from spire.doc import *
 from spire.doc.common import *
 import pandas as pd
 import json
+import logging
 
 def read_json():
-    # Open and read the JSON file
-    with open('config.json', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+#
+#
+    logging.basicConfig(level=logging.INFO, filename="runtime.log", filemode="w")
+    try:
+
+        with open('config.json', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        logging.exception(str(str(e)))
 def csv(json):
-    csv = pd.read_csv(json['csvFilePath'])
-    return csv
+#
+#
+    logging.basicConfig(level=logging.INFO, filename="runtime.log", filemode="w")
+    try:
+        csv = pd.read_csv(json['csvFilePath'])
+        return csv
+    except Exception as e:
+        logging.exception(str(str(e)))
 def document(df, json):
 #
 #
-    # Create a Document object
-    n = len(df.index)
-    doc = Document()
+    logging.basicConfig(level=logging.INFO, filename="runtime.log", filemode="w")
+    try:
 
-    # Add a section
-    section = doc.AddSection()
-    # Create a table
+        n = len(df.index)
+        doc = Document()
 
-    titleParagraph = section.AddParagraph()
-    titleParagraph.AppendText(str(json['documentTitle']))
-    titleParagraph.ApplyStyle(BuiltinStyle.Heading1)
-    titleParagraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+        section = doc.AddSection()
 
-    table = section.AddTable(True)
+        titleParagraph = section.AddParagraph()
+        titleParagraph.AppendText(str(json['documentTitle']))
+        titleParagraph.ApplyStyle(BuiltinStyle.Heading1)
+        titleParagraph.Format.HorizontalAlignment = HorizontalAlignment.Center
 
-    # Set the row number and column number of table
-
-    table.ResetCells(n + 2, 4)
-
-    # Set the width of table
-
-    table.PreferredWidth = PreferredWidth(WidthType.Percentage, int(100))
+        table = section.AddTable(True)
 
 
-    # Set row height
+        table.ResetCells(n + 2, 4)
 
-    for i in range(0, table.Rows.Count):
+        table.PreferredWidth = PreferredWidth(WidthType.Percentage, int(100))
 
-        table.Rows[i].Height = 20.0
+        for i in range(0, table.Rows.Count):
+            table.Rows[i].Height = 20.0
 
-    # Horizontally merge cells
+        table.ApplyHorizontalMerge(0, 0, 3)
 
-    table.ApplyHorizontalMerge(0, 0, 3)
-
-    # Get a cell
-
-
-    cell = table.Rows[1].Cells[0]
-    cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
-    paragraph = cell.AddParagraph()
-    paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-    paragraph.AppendText('№')
-
-
-    cell = table.Rows[1].Cells[1]
-    cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
-    paragraph = cell.AddParagraph()
-    paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-    paragraph.AppendText(df.columns[0])
-
-
-    cell = table.Rows[1].Cells[2]
-    cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
-    paragraph = cell.AddParagraph()
-    paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-    paragraph.AppendText(df.columns[1])
-
-
-    cell = table.Rows[1].Cells[3]
-    cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
-    paragraph = cell.AddParagraph()
-    paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-    paragraph.AppendText(df.columns[2])
-
-
-    cell = table.Rows[0].Cells[0]
-    cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
-    paragraph = cell.AddParagraph()
-    paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-    paragraph.AppendText('Протокол')
-
-    for i in range(2, n + 2):
-        cell = table.Rows[i].Cells[0]
+        cell = table.Rows[1].Cells[0]
         cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
         paragraph = cell.AddParagraph()
         paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-        paragraph.AppendText(str(i - 1))
+        paragraph.AppendText('№')
 
-        cell = table.Rows[i].Cells[1]
+        cell = table.Rows[1].Cells[1]
         cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
         paragraph = cell.AddParagraph()
         paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-        paragraph.AppendText(str(df['Фамилия'][i - 2]))
+        paragraph.AppendText(df.columns[0])
 
-        cell = table.Rows[i].Cells[2]
+        cell = table.Rows[1].Cells[2]
         cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
         paragraph = cell.AddParagraph()
         paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-        paragraph.AppendText(str(df['Имя'][i - 2]))
+        paragraph.AppendText(df.columns[1])
 
-        cell = table.Rows[i].Cells[3]
+        cell = table.Rows[1].Cells[3]
         cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
         paragraph = cell.AddParagraph()
         paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
-        paragraph.AppendText(str(df['Оценка'][i - 2]))
+        paragraph.AppendText(df.columns[2])
 
-    bodyParagraph_1 = section.AddParagraph()
-    bodyParagraph_1.AppendText(' ')
-    # Split the cell into 3 smaller cells
+        cell = table.Rows[0].Cells[0]
+        cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
+        paragraph = cell.AddParagraph()
+        paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+        paragraph.AppendText('Протокол')
 
+        for i in range(2, n + 2):
+            cell = table.Rows[i].Cells[0]
+            cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
+            paragraph = cell.AddParagraph()
+            paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+            paragraph.AppendText(str(i - 1))
 
-    bodyParagraph_2 = section.AddParagraph()
-    bodyParagraph_2.AppendText('Обучение провел ' + str(json['employee']['position']) +
-                               ' ' + str(json['employee']['lastName']) +
-                               ' ' + str(json['employee']['firstName'][0]) + '.' +
-                               ' ' + str(json['employee']['middleName'][0]) + '.' +
-                               ' ' + '______________(подпись)')
+            cell = table.Rows[i].Cells[1]
+            cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
+            paragraph = cell.AddParagraph()
+            paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+            paragraph.AppendText(str(df['Фамилия'][i - 2]))
 
+            cell = table.Rows[i].Cells[2]
+            cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
+            paragraph = cell.AddParagraph()
+            paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+            paragraph.AppendText(str(df['Имя'][i - 2]))
 
-    doc.SaveToFile("output/Протокол обучения пользователей №1.docx", FileFormat.Docx2013)
+            cell = table.Rows[i].Cells[3]
+            cell.CellFormat.VerticalAlignment = VerticalAlignment.Middle
+            paragraph = cell.AddParagraph()
+            paragraph.Format.HorizontalAlignment = HorizontalAlignment.Center
+            paragraph.AppendText(str(df['Оценка'][i - 2]))
 
-    doc.Close()
+        bodyParagraph_1 = section.AddParagraph()
+        bodyParagraph_1.AppendText(' ')
 
+        bodyParagraph_2 = section.AddParagraph()
+        bodyParagraph_2.AppendText('Обучение провел ' + str(json['employee']['position']) +
+                                    ' ' + str(json['employee']['lastName']) +
+                                    ' ' + str(json['employee']['firstName'][0]) + '.' +
+                                    ' ' + str(json['employee']['middleName'][0]) + '.' +
+                                    ' ' + '______________(подпись)')
+
+        doc.SaveToFile("output/Протокол обучения пользователей №1.docx", FileFormat.Docx2013)
+
+        doc.Close()
+    except Exception as e:
+        logging.exception(str(str(e)))
 
 if __name__ == "__main__":
     json = read_json()
